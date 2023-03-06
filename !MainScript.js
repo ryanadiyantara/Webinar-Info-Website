c632474b4e4e272eb690f1d0d348d88b6b1da7e8
Ahbrew <adiyantararyan@gmail.com>
2023-03-07 06:07:22 +0700

    Add files via upload

---
+++ !MainScript.js
@@ -0,0 +1,35 @@
+/* SCRIPT HEADER */
+function btnDaftar(){
+    alert("Fitur ini belum tersedia!");
+}
+
+function btnMasuk(){
+    alert("Fitur ini belum tersedia!");
+}
+
+/* SCRIPT FILTER */
+function btnReset(){
+    alert("Fitur ini belum tersedia!");
+}
+
+function btnSearch(){
+    alert("Fitur ini belum tersedia!");
+}
+
+/* SCRIPT FOOTER */
+function btnWhatsapp(){
+    alert("Fitur ini belum tersedia!");
+}
+
+function btnInstagram(){
+    alert("Fitur ini belum tersedia!");
+}
+
+function btnTelegram(){
+    alert("Fitur ini belum tersedia!");
+}
+
+/* SCRIPT CONTENT DAFTAR */
+function btnDaftarWebinar(){
+    alert("Silahkan Login Terlebih Dahulu!");
+}
